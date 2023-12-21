25bcf330629c9ea066996e814551450962280025
Aman Dhillon <a.dhillon9779@gmail.com>
2023-12-21 11:26:01 +0530

    Making corousel

--- imdb-clone/src/pages/Home.jsx
+++ imdb-clone/src/pages/Home.jsx
@@ -1,12 +1,31 @@
-
+import { useEffect, useState } from "react";
 
 //components
 import Header from "../components/common/Header";
+import Banner from "../components/Banner";
+
+import { categoryMovies } from "../services/api";
+import { NOWPLAYING_API_URL } from "../constants/constant";
+import { Box } from "@mui/material";
 
 const Home = () => {
+  const [movies, setMovies] = useState([]);
 
-    return(
-        <Header />
-    )
-}
+  useEffect(() => {
+    const getData = async () => {
+      let response = await categoryMovies(NOWPLAYING_API_URL);
+      setMovies(response.results);
+    };
+    getData();
+  }, []);
+
+  return (
+  <div>
+    <Header />
+    <Box>
+      <Banner />
+    </Box>
+  </div>
+  );
+};
 export default Home;
